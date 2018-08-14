bbfe42ae4097dc20a8946a95f42112bea0b82a4a
Mohammad Hajiaghazadeh <mohammad.hajiaghazadeh@gmail.com>
2018-08-14 16:39:21 +0430

    feat: react pages meta and title from config

--- src/app/@core/services/page-title-tags.service.ts
+++ src/app/@core/services/page-title-tags.service.ts
@@ -3,17 +3,35 @@
  */
 import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 import {Meta, Title} from "@angular/platform-browser";
-import {Injectable} from "@angular/core";
-import {PAGE_TITLE_TAGS_CONST} from "../../config/tags/page-title-tags.const";
+import {Injectable, OnInit} from "@angular/core";
 import {getNestedValue} from "../utils/nested-object-value";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable()
-export class PageTitleTagsService {
+export class PageTitleTagsService implements OnInit {
+  private titleTags: any;
+  private JSON_DIR: string = '/assets/config/title-tags/title-tags.config.json';
+
   constructor(private readonly router: Router,
               private readonly activatedRoute: ActivatedRoute,
               private titleService: Title,
+              private httpClient: HttpClient,
               private meta: Meta) {
 
+  }
+
+  ngOnInit() {
+    this.loadTitleTags();
+  }
+
+
+  /**
+   * load title tags
+   */
+  loadTitleTags() {
+    this.httpClient.get(this.JSON_DIR).subscribe(titleTags => {
+      this.titleTags = titleTags;
+    })
   }
 
   /**
@@ -32,14 +50,18 @@ export class PageTitleTagsService {
       .filter((route) => route.outlet === 'primary')
       .mergeMap((route) => route.data)
       .subscribe((event) => {
-        if (event['TITLE_TAG_KEY']) {
-          let PAGE_TITLE_TAGS = getNestedValue(PAGE_TITLE_TAGS_CONST, event['TITLE_TAG_KEY']);
-          PAGE_TITLE_TAGS['metaTags'].forEach((tag: any) => {
-            this.meta.updateTag(tag, `name='${tag.name}'`);
-          });
-          this.titleService.setTitle(PAGE_TITLE_TAGS['title']);
+        if (this.titleTags) {
+          if (event['TITLE_TAG_KEY']) {
+            let PAGE_TITLE_TAGS = getNestedValue(this.titleTags, event['TITLE_TAG_KEY']);
+            PAGE_TITLE_TAGS['metaTags'].forEach((tag: any) => {
+              this.meta.updateTag(tag, `name='${tag.name}'`);
+            });
+            this.titleService.setTitle(PAGE_TITLE_TAGS['title']);
+          } else {
+            this.titleService.setTitle('NG_IR')
+          }
         } else {
-          this.titleService.setTitle('NG_IR')
+          this.loadTitleTags();
         }
       });
   }
--- src/app/config/interceptors/notification.interceptor.ts
+++ src/app/config/interceptors/notification.interceptor.ts
@@ -6,10 +6,9 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
-import {CONFIG} from '../config'
 import {BodyOutputType, Toast, ToasterService} from "angular2-toaster";
 
-const exceptionList: Array<any> = ['/assets/i18n/en.json', '/assets/i18n/fa.json'];
+const exceptionList: Array<any> = ['/assets/'];
 
 @Injectable()
 export class NotificationInterceptor implements HttpInterceptor {
@@ -17,9 +16,16 @@ export class NotificationInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const {url} = request;
     return next.handle(request).map((event: any) => {
       if (event && event.body) {
-        if (event.url.indexOf(exceptionList[0]) === -1 && event.url.indexOf(exceptionList[1]) === -1) {
+        let isUrlException: boolean = false;
+        exceptionList.forEach((exception) => {
+          if (url.includes(exception)) {
+            isUrlException = true;
+          }
+        });
+        if (!isUrlException) {
           this.popNotification('با موفقیت انجام شد', event.body.message);
         }
       }
