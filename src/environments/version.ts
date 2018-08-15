fda9106c5463b5f60ef0f2a3296124a0c6d45f7f
Mohammad Hajiaghazadeh <mohammad.hajiaghazadeh@gmail.com>
2018-08-15 15:30:28 +0430

    update

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,21 +2,13 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "v0.1.0-8-g6efa96d-dirty",
-    "hash": "g6efa96d",
-    "distance": 8,
-    "tag": "v0.1.0",
-    "semver": {
-        "raw": "v0.1.0",
-        "major": 0,
-        "minor": 1,
-        "patch": 0,
-        "prerelease": [],
-        "build": [],
-        "version": "0.1.0"
-    },
-    "suffix": "8-g6efa96d-dirty",
-    "semverString": "0.1.0+8.g6efa96d",
-    "version": "1.11.3"
+    "raw": "v1.11-21-ga4b262e-dirty",
+    "hash": "ga4b262e",
+    "distance": 21,
+    "tag": "v1.11",
+    "semver": null,
+    "suffix": "21-ga4b262e-dirty",
+    "semverString": "",
+    "version": "1.12.5"
 };
 /* tslint:enable */
