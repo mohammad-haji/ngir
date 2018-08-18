32e44170e632778fa0e00240e82e1841fa9f25d4
Mohammad Hajiaghazadeh <mohammad.hajiaghazadeh@gmail.com>
2018-08-18 15:41:57 +0430

    feat: add api date cleaner service

---
+++ src/app/@core/services/api/api-data-cleaner.service.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by Mohammad.hajiaghazadeh on 8/15/2018
+ */
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class ApiDataCleanerService {
+  constructor() {
+  }
+
+  cleanForCreate(entity: string, data: any): any {
+
+  }
+
+  cleanForUpdate(): any {
+
+  }
+}
