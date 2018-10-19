9f4da2603e705dad22ce049a5658427d29ab8b98
mohammad.haji <mohammad.hajiaghazadeh@gmail.com>
2018-10-19 10:10:34 +0330

    finish about-us

--- src/app/public/about-us/about-us.module.ts
+++ src/app/public/about-us/about-us.module.ts
@@ -7,13 +7,13 @@ import {AboutUsComponent} from "./about-us.component";
 import {AboutUsRoutingModule} from "./about-us-routing.module";
 import {ThemeModule} from "../../@theme/theme.module";
 import {NgxCommon} from "../../common/common.module";
-// import {NguCarouselModule} from "@ngu/carousel";
+import {NguCarouselModule} from "@ngu/carousel";
 @NgModule({
   imports: [
     AboutUsRoutingModule,
     ThemeModule,
     NgxCommon,
-    // NguCarouselModule
+    NguCarouselModule
   ],
   declarations: [
     AboutUsComponent
